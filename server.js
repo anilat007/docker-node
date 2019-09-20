3f49575e93f3e86cc64ee9f60329cc55e116e4b4
Anil Darji <anil.darji@cdk.com>
2019-09-20 15:24:37 -0700

    Added few new endpoints

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 'use strict';
 const os = require('os');
-const querystring = require('querystring');
 const express = require('express');
 
 // Constants
@@ -17,24 +16,35 @@ app.get('/host', (req, res) => {
   res.send('host: ' + os.hostname() +'\n');
 });
 
-app.get('/os', (req, res) => {
-  var response = {};
-  for(var q in req.query){
-	if(q == "cpu")
-		response[q] = os.cpus();
-	else if(q == "hostname")
-		response[q] = os.hostname();
-	else if(q == "const")
-		response[q] = os.constants;
-	else if(q == "arch")
-		response[q] = os.arch();
-	else if(q == "freemem")
-		response[q] = os.freemem();
-	else if(q == "platform")
-		response[q] = os.platform();
-  } 
-	res.send(JSON.stringify(response) +'\n');  
+app.get('/time', (req, res) => {
+  res.send((new Date()).toUTCString() +'\n');
 });
+
+app.get('/echo', (req, res) => {
+	if(!req.query.text)
+		res.send("Usage: http://baseurl/echo?text=sayIt" + "\n");
+	else
+  	res.send(req.query.text +'\n');
+});
+
+// app.get('/os', (req, res) => {
+//   var response = {};
+//   for(var q in req.query){
+// 	if(q == "cpu")
+// 		response[q] = os.cpus();
+// 	else if(q == "hostname")
+// 		response[q] = os.hostname();
+// 	else if(q == "const")
+// 		response[q] = os.constants;
+// 	else if(q == "arch")
+// 		response[q] = os.arch();
+// 	else if(q == "freemem")
+// 		response[q] = os.freemem();
+// 	else if(q == "platform")
+// 		response[q] = os.platform();
+//   } 
+// 	res.send(JSON.stringify(response) +'\n');  
+// });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
